4633cfc9a56b4b5e7eef695fc3d4349c0edb4f4c
eiroca <39483731+eiroca@users.noreply.github.com>
2019-06-04 17:07:35 +0200

    New features

--- eLibDynatrace/src/main/java/net/eiroca/library/dynatrace/DynatracePluginUtils.java
+++ eLibDynatrace/src/main/java/net/eiroca/library/dynatrace/DynatracePluginUtils.java
@@ -35,17 +35,17 @@ public class DynatracePluginUtils {
   static SortedMap<String, SortedMap<String, MetricMetadata>> definition = new TreeMap<>();
   private static final Map<String, String> ALIAS = new HashMap<>();
   static {
-    DynatracePluginUtils.ALIAS.put("number", null);
-    DynatracePluginUtils.ALIAS.put("num", null);
-    DynatracePluginUtils.ALIAS.put("unit", null);
-    DynatracePluginUtils.ALIAS.put("boolean", null);
-    DynatracePluginUtils.ALIAS.put("counter", null);
-    DynatracePluginUtils.ALIAS.put("purepath", null);
+    DynatracePluginUtils.ALIAS.put("number", "number");
+    DynatracePluginUtils.ALIAS.put("num", "number");
+    DynatracePluginUtils.ALIAS.put("unit", "number");
+    DynatracePluginUtils.ALIAS.put("boolean", "number");
+    DynatracePluginUtils.ALIAS.put("counter", "number");
+    DynatracePluginUtils.ALIAS.put("purepath", "number");
     DynatracePluginUtils.ALIAS.put("rate", "number");
-    DynatracePluginUtils.ALIAS.put("request", null);
-    DynatracePluginUtils.ALIAS.put("requests", null);
-    DynatracePluginUtils.ALIAS.put("operation", null);
-    DynatracePluginUtils.ALIAS.put("operations", null);
+    DynatracePluginUtils.ALIAS.put("request", "number");
+    DynatracePluginUtils.ALIAS.put("requests", "number");
+    DynatracePluginUtils.ALIAS.put("operation", "number");
+    DynatracePluginUtils.ALIAS.put("operations", "number");
   }
 
   public static void addMetadata(final String groupName, final String metricName, final MetricMetadata metadata) {
@@ -59,58 +59,67 @@ public class DynatracePluginUtils {
 
   public static void main(final String[] args) {
     final String baseName = "net.eiroca.dynatrace.plugins.AdvancedMonitor";
-    final String dynaMonitorName = baseName + ".monitor";
     for (final String monitorname : ServerMonitors.registry.getNames()) {
       if (monitorname.equals(ServerMonitors.registry.defaultName())) {
         continue;
       }
-      IServerMonitor monitor;
-      try {
-        monitor = ServerMonitors.build(monitorname);
-      }
-      catch (final Exception e) {
-        e.printStackTrace();
-        continue;
-      }
-      final List<MetricGroup> groups = new ArrayList<>();
-      monitor.loadMetricGroup(groups);
-      for (final MetricGroup mg : groups) {
-        final String mgName = mg.getName();
-        for (final IMetric<?> m : mg.getMetrics()) {
-          final MetricMetadata meta = m.getMetadata();
-          final String mName = meta.getDisplayName();
-          if (mName.contains("Rows rocessed")) {
-            // System.err.println(monitorname);
-          }
-          DynatracePluginUtils.addMetadata(mgName, mName, meta);
-        }
-      }
+      processMonitor(monitorname);
     }
     final StringBuilder sb = new StringBuilder(1024);
     int num = 0;
     for (final Entry<String, SortedMap<String, MetricMetadata>> groups : DynatracePluginUtils.definition.entrySet()) {
       num++;
-      final String mgName = groups.getKey();
-      final String sectionName = baseName + ".metricgroup_" + num;
-      sb.append("<extension point=\"com.dynatrace.diagnostics.pdk.monitormetricgroup\" id=\"" + sectionName + "\" name=\"" + mgName + "\">").append(LibStr.NL);
-      sb.append(" <metricgroup monitorid=\"" + dynaMonitorName + "\">").append(LibStr.NL);
-      for (final Entry<String, MetricMetadata> metrics : groups.getValue().entrySet()) {
-        final MetricMetadata meta = metrics.getValue();
-        sb.append("  <metric");
-        DynatracePluginUtils.addAttribute(sb, "name", meta.getDisplayName(), "?", (Map<String, String>)null);
-        DynatracePluginUtils.addAttribute(sb, "description", meta.getDescription(), meta.getInternalName(), null);
-        DynatracePluginUtils.addAttribute(sb, "unit", meta.getUnit(), null, DynatracePluginUtils.ALIAS);
-        DynatracePluginUtils.addAttribute(sb, "defaultrate", meta.getRate(), null, DynatracePluginUtils.ALIAS);
-        DynatracePluginUtils.addAttribute(sb, "hidedisplayaggregation", meta.getNoagg(), null, DynatracePluginUtils.ALIAS);
-        if (meta.getCalcDelta()) {
-          DynatracePluginUtils.addAttribute(sb, "calculatedelta", "true", null, null);
-        }
-        sb.append(" />").append(LibStr.NL);
-      }
-      sb.append(" </metricgroup>").append(LibStr.NL);
-      sb.append("</extension>").append(LibStr.NL);
+      exportEntry(sb, groups, baseName, num);
     }
     System.out.println(sb.toString());
+  }
+
+  private static void exportEntry(StringBuilder sb, Entry<String, SortedMap<String, MetricMetadata>> groups, String baseName, int num) {
+    final String mgName = groups.getKey();
+    final String sectionName = baseName + ".metricgroup_" + num;
+    sb.append("<extension point=\"com.dynatrace.diagnostics.pdk.monitormetricgroup\" id=\"" + sectionName + "\" name=\"" + mgName + "\">").append(LibStr.NL);
+    sb.append(" <metricgroup monitorid=\"" + baseName + ".monitor\">").append(LibStr.NL);
+    for (final Entry<String, MetricMetadata> metrics : groups.getValue().entrySet()) {
+      final MetricMetadata meta = metrics.getValue();
+      sb.append("  <metric");
+      DynatracePluginUtils.addAttribute(sb, "name", meta.getDisplayName(), "?", (Map<String, String>)null);
+      DynatracePluginUtils.addAttribute(sb, "description", meta.getDescription(), meta.getInternalName(), null);
+      DynatracePluginUtils.addAttribute(sb, "unit", meta.getUnit(), "number", DynatracePluginUtils.ALIAS);
+      DynatracePluginUtils.addAttribute(sb, "defaultrate", meta.getRate(), null, DynatracePluginUtils.ALIAS);
+      DynatracePluginUtils.addAttribute(sb, "hidedisplayaggregation", meta.getNoagg(), null, DynatracePluginUtils.ALIAS);
+      if (meta.getCalcDelta()) {
+        DynatracePluginUtils.addAttribute(sb, "calculatedelta", "true", null, null);
+      }
+      sb.append(" />").append(LibStr.NL);
+    }
+    sb.append(" </metricgroup>").append(LibStr.NL);
+    sb.append("</extension>").append(LibStr.NL);
+  }
+
+  private static void processMonitor(String monitorname) {
+    IServerMonitor monitor;
+    try {
+      monitor = ServerMonitors.build(monitorname);
+    }
+    catch (final Exception e) {
+      e.printStackTrace();
+      return;
+    }
+    final List<IMetric<?>> metrics = new ArrayList<>();
+    MetricGroup monitorMG = monitor.getMetricGroup();
+    for (final MetricGroup mg : monitorMG.getGroups()) {
+      final String mgName = mg.getName();
+      metrics.clear();
+      mg.loadMetrics(metrics, true);
+      for (final IMetric<?> m : metrics) {
+        final MetricMetadata meta = m.getMetadata();
+        final String mName = meta.getDisplayName();
+        if (mName.contains("Rows rocessed")) {
+          // System.err.println(monitorname);
+        }
+        DynatracePluginUtils.addMetadata(mgName, mName, meta);
+      }
+    }
   }
 
   private static void addAttribute(final StringBuilder sb, final String name, String val, final String defVal, final Map<String, String> renamed) {
