7d1356ffed3775614fc051e6e3f26c8759748f17
eiroca <39483731+eiroca@users.noreply.github.com>
2020-02-23 12:13:18 +0100

    Fixes

--- update.bat
+++ update.bat
@@ -1,29 +1,29 @@
 for /D %%d in (AppMon-Plugin-Monitor-*) do (
 	del /Q .\%%d\lib\*
-	xcopy /y .\eLibAppMonPlugin\target\eLibAppMonPlugin-0.0.8.jar .\%%d\lib
+	xcopy /y .\eLibAppMonPlugin\target\eLibAppMonPlugin-0.0.9.jar .\%%d\lib
 	xcopy /y .\eLibAppMonPlugin\target\libs\* .\%%d\lib
 )
 
 for /D %%d in (AppMon-Plugin-Task-*) do (
 	del /Q .\%%d\lib\*
-	xcopy /y .\eLibAppMonPlugin\target\eLibAppMonPlugin-0.0.8.jar .\%%d\lib
+	xcopy /y .\eLibAppMonPlugin\target\eLibAppMonPlugin-0.0.9.jar .\%%d\lib
 	xcopy /y .\eLibAppMonPlugin\target\libs\* .\%%d\lib
 )
 
 for /D %%d in (AppMon-Plugin-Action-*) do (
 	del /Q .\%%d\lib\*
-	xcopy /y .\eLibAppMonPlugin\target\eLibAppMonPlugin-0.0.8.jar .\%%d\lib
+	xcopy /y .\eLibAppMonPlugin\target\eLibAppMonPlugin-0.0.9.jar .\%%d\lib
 	xcopy /y .\eLibAppMonPlugin\target\libs\* .\%%d\lib
 )
 
-xcopy /y ..\..\library\eJDBCDrivers\libs\* .\AppMon-Plugin-Monitor-OracleServer\lib
-xcopy /y ..\..\library\eJDBCDrivers\target\libs\* .\AppMon-Plugin-Monitor-OracleServer\lib
+xcopy /y ..\..\library\eJDBCDrivers\libs\oracle\* .\AppMon-Plugin-Monitor-OracleServer\lib
 
-xcopy /y ..\..\library\eJDBCDrivers\libs\* .\AppMon-Plugin-Monitor-DatabaseServer\lib
+xcopy /y ..\..\library\eJDBCDrivers\libs\db2\*    .\AppMon-Plugin-Monitor-DatabaseServer\lib
+xcopy /y ..\..\library\eJDBCDrivers\libs\oracle\* .\AppMon-Plugin-Monitor-DatabaseServer\lib
 xcopy /y ..\..\library\eJDBCDrivers\target\libs\* .\AppMon-Plugin-Monitor-DatabaseServer\lib
 
-xcopy /y ..\..\library\eJDBCDrivers\libs\* .\AppMon-Plugin-Monitor-PostgresServer\lib
-xcopy /y ..\..\library\eJDBCDrivers\target\libs\* .\AppMon-Plugin-Monitor-PostgresServer\lib
+xcopy /y ..\..\library\eJDBCDrivers\target\libs\postgresql-* .\AppMon-Plugin-Monitor-PostgresServer\lib
 
-xcopy /y ..\..\library\eJDBCDrivers\libs\* .\AppMon-Plugin-Monitor-Advanced\lib
+xcopy /y ..\..\library\eJDBCDrivers\libs\db2\*    .\AppMon-Plugin-Monitor-Advanced\lib
+xcopy /y ..\..\library\eJDBCDrivers\libs\oracle\* .\AppMon-Plugin-Monitor-Advanced\lib
 xcopy /y ..\..\library\eJDBCDrivers\target\libs\* .\AppMon-Plugin-Monitor-Advanced\lib
